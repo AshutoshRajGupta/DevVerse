c992fb2c86327c06e1d6c0a9fe644703884539c8
Ashutosh Raj Gupta <ag2364443@gmail.com>
2026-04-24 23:22:22 +0530

    feat(home Page): Added layout for it and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 export default function Home() {
   return (
     <section>
       <h1 className='text-center mt-4'>DevVerse - The Hub for Every Dev <br /> Events You can't Miss!!</h1>
         <p className='text-center mt-5'> Hackathons, Meetups, and Conferences All in One Place</p>
+        <ExploreBtn />
     </section>
   );
 }
