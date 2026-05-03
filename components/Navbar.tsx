216d6b4c72459c46cd06735b05958175edfe5563
Ashutosh Raj Gupta <ag2364443@gmail.com>
2026-05-03 12:49:44 +0530

    feat(final completion): Completed with the final event details page and also configure posthog for web analytics and performance measure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,8 +12,8 @@ const Navbar = () => {
 
                 <ul>
                     <Link href='/'>Home</Link>
-                    <Link href='/events'>Events</Link>
-                    <Link href='/'>Create Event</Link>
+                    <Link href='/about'>About</Link>
+                    {/*<Link href='/'>Create Event</Link>*/}
                 </ul>
             </nav>
         </header>
